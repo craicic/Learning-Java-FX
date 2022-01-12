4f4276976990b8b62d631cf17662eea436fbef34
xxjokerx <common.hori@protonmail.com>
2022-01-12 14:12:44 +0100

    Training scenes

--- src/main/java/me/vaalp/learningjavafx/MainApplication.java
+++ src/main/java/me/vaalp/learningjavafx/MainApplication.java
@@ -4,8 +4,12 @@ package me.vaalp.learningjavafx;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainApplication extends javafx.application.Application {
@@ -18,19 +22,47 @@ public class MainApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, Color.GRAY);
-
+        Scene scene = new Scene(root, 600,600, Color.GRAY);
         Image icon = new Image("icon.png");
-
         stage.setTitle("Stage Demo Program");
         stage.getIcons().add(icon);
+
+        Text text = new Text();
+        text.setText("WHOOOOOOO!!!");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Consolas",50));
+        text.setFill(Color.GOLD);
+
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(500);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.WHITE);
+        line.setRotate(45);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(150);
+        rectangle.setY(50);
+        rectangle.setWidth(150);
+        rectangle.setHeight(250);
+        rectangle.setFill(Color.GOLD);
+        rectangle.setStrokeWidth(5);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+                200.0,200.0,
+                300.0,300.0,
+                350.0,400.0);
+        triangle.setFill(Color.DARKGREEN);
+
+        root.getChildren().add(triangle);
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
         stage.setScene(scene);
-        stage.setWidth(420);
-        stage.setHeight(450);
-        stage.setResizable(false);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitHint("Haha you're stuck unless you press 'a'");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("a"));
         stage.show();
     }
 }
