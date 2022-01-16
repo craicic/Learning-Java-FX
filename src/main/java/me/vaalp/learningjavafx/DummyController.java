20babbffcf900b603dfbdd1ae68bdf1b07818908
xxjokerx <common.hori@protonmail.com>
2022-01-16 20:26:33 +0100

    Refactoring MainApplication class

--- src/main/java/me/vaalp/learningjavafx/DummyController.java
+++
@@ -1,4 +0,0 @@
-package me.vaalp.learningjavafx;
-
-public class DummyController {
-}
--- src/main/java/me/vaalp/learningjavafx/MainApplication.java
+++ src/main/java/me/vaalp/learningjavafx/MainApplication.java
@@ -9,15 +9,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.List;
+
 public class MainApplication extends javafx.application.Application {
 
+
+    private Rectangle rectangle;
+    private Timeline timeline;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,65 +31,81 @@ public class MainApplication extends javafx.application.Application {
     public void start(Stage stage) {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.GRAY);
-        Image icon = new Image("icon.png");
+
+        createRectangle();
+        setAnimation();
+
+        root.getChildren().add(rectangle);
+        root.getChildren().add(createHBox(
+                List.of(
+                        createRotateBtn(),
+                        createAutoRotateBtn(),
+                        createStopBtn())));
+
         stage.setTitle("Transform Shape Program");
-        stage.getIcons().add(icon);
+        stage.getIcons().add(new Image("icon.png"));
+        stage.setScene(scene);
+        stage.show();
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(400);
-        rectangle.setY(290);
-        rectangle.setWidth(50);
-        rectangle.setHeight(20);
-        rectangle.setFill(Color.GOLD);
-        rectangle.setStrokeWidth(5);
+    }
 
-
-        KeyValue kv = new KeyValue(rectangle.rotateProperty(), rectangle.getRotate() + 1440.0);
-        KeyFrame kf = new KeyFrame(Duration.seconds(1.0), kv);
-        Timeline timeline = new Timeline(kf);
-
+    private void setAnimation() {
+        timeline = new Timeline(createKeyFrame());
         timeline.setOnFinished(event -> rectangle.setRotate(0));
+    }
 
-        Button button1 = new Button("Rotate shape");
-        button1.setOnAction(event -> rotate(rectangle));
+    private KeyFrame createKeyFrame() {
+        return new KeyFrame(Duration.seconds(10), createKeyValue());
+    }
 
-        Button button2 = new Button("Let's GO !");
-        button2.setOnAction(event -> timeline.play());
+    private KeyValue createKeyValue() {
+        return new KeyValue(rectangle.rotateProperty(), rectangle.getRotate() + 1440.0);
+    }
 
+    private Button createStopBtn() {
         Button button3 = new Button("Stop this madness !");
         button3.setOnAction(event -> {
                     timeline.stop();
                     rectangle.setRotate(0);
                 }
         );
+        return button3;
+    }
 
-        Text marker = new Text(String.valueOf(timeline.getCycleCount()));
+    private Button createAutoRotateBtn() {
+        Button btn = new Button("Let's GO !");
+        btn.setOnAction(event -> timeline.play());
+        return btn;
+    }
 
+    private Button createRotateBtn() {
+        Button btn = new Button("Rotate shape");
+        btn.setOnAction(event -> rotate(rectangle));
+        return btn;
+    }
+
+    private void createRectangle() {
+        rectangle = new Rectangle();
+        rectangle.setX(300);
+        rectangle.setY(290);
+        rectangle.setWidth(50);
+        rectangle.setHeight(20);
+        rectangle.setStrokeWidth(5);
+        rectangle.setFill(Color.GOLD);
+    }
+
+    private HBox createHBox(List<Region> regions) {
         HBox hbox = new HBox();
-        hbox.getChildren().add(button1);
-        hbox.getChildren().add(button2);
-        hbox.getChildren().add(button3);
-        hbox.getChildren().add(marker);
-
+        for (Region region : regions) {
+            hbox.getChildren().add(region);
+        }
         hbox.setSpacing(20.0);
-
-
-        Polygon triangle = new Polygon();
-        triangle.getPoints().setAll(
-                200.0, 200.0,
-                300.0, 300.0,
-                350.0, 400.0);
-        triangle.setFill(Color.DARKGREEN);
-
-        root.getChildren().add(rectangle);
-        root.getChildren().add(hbox);
-
-        stage.setScene(scene);
-        stage.show();
-
+        return hbox;
     }
 
     private void rotate(Rectangle box) {
         box.setRotate(box.getRotate() + 15);
     }
+
+
 }
